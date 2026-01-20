61010b0d2b1fb78083571c57e5ebad46c6974848
wiki <474464056@qq.com>
2026-01-21 00:01:28 +0800

    完成了LCTHW_ex29

---
+++ E4/src/libex29.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "dbg.h"
+
+
+int print_a_message(const char *msg, int len)
+{
+    printf("A STRING: %s\n", msg);
+
+    return 0;
+}
+
+
+int uppercase(const char *msg, int len)
+{
+    int i = 0;
+
+    // BUG: \0 termination problems
+    for(i = 0; i < len; i++) {
+        printf("%c", toupper(msg[i]));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+int lowercase(const char *msg, int len)
+{
+    int i = 0;
+
+    // BUG: \0 termination problems
+    for(i = 0; i < len; i++) {
+        printf("%c", tolower(msg[i]));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+int fail_on_purpose(const char *msg, int len)
+{
+    return 1;
+}
