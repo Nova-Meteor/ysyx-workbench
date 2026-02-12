7c3a466da88f8c836f6c5d26168d3e40196945e2
tracer-ysyx <tracer@ysyx.org>
2026-02-13 00:07:16 +0800

    > sim RTL ysyx_22040000 黄伟健 Linux wiki-X99 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:07:16 up  2:42,  1 user,  load average: 2.49, 2.65, 2.56

--- npc/ex2/vsrc/ex2.v
+++ npc/ex2/vsrc/ex2.v
@@ -1,7 +1,14 @@
 module ex2(
     input [8:0] sw,      // SW[7:0] 为输入, SW[8] 为使能
     output [3:0] led,    // LED[2:0] 为编码结果, LED[3] 为指示位
-    output [7:0] seg0    // 数码管 HEX0
+    output [7:0] seg0,    // 数码管 HEX0
+    output [7:0] seg1,    // 数码管 HEX1
+    output [7:0] seg2,    // 数码管 HEX2
+    output [7:0] seg3,    // 数码管 HEX3
+    output [7:0] seg4,    // 数码管 HEX4
+    output [7:0] seg5,    // 数码管 HEX5
+    output [7:0] seg6,    // 数码管 HEX6
+    output [7:0] seg7     // 数码管 HEX7
   );
 
   wire [2:0] encode_out;
@@ -20,6 +27,41 @@ module ex2(
   bcd7seg my_seg(
             .b({1'b0, encode_out}),
             .h(seg0)
+          );
+
+  bcd7seg my_seg1(
+            .b(4'b0),
+            .h(seg1)
+          );
+
+  bcd7seg my_seg2(
+            .b(4'b0),
+            .h(seg2)
+          );
+
+  bcd7seg my_seg3(
+            .b(4'b0),
+            .h(seg3)
+          );
+
+  bcd7seg my_seg4(
+            .b(4'b0),
+            .h(seg4)
+          );
+
+  bcd7seg my_seg5(
+            .b(4'b0),
+            .h(seg5)
+          );
+
+  bcd7seg my_seg6(
+            .b(4'b0),
+            .h(seg6)
+          );
+
+  bcd7seg my_seg7(
+            .b(4'b0),
+            .h(seg7)
           );
 
 endmodule
