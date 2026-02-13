739d7689d88f6962b840a103e7bf8912ddc70dc7
tracer-ysyx <tracer@ysyx.org>
2026-02-13 21:51:40 +0800

    > sim RTL ysyx_22040000 黄伟健 Linux wiki-X99 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:40 up  2:40,  1 user,  load average: 4.37, 4.33, 3.49

---
+++ npc/scpu/vsrc/cpu.v
@@ -0,0 +1,88 @@
+module cpu(
+    input clk,
+    input rst,
+    output reg [7:0] out_val
+  );
+
+  reg [3:0] pc;
+  reg [7:0] inst;
+
+  // 1. 指令存储器 (IMEM)
+  // 放入你提供的汇编机器码
+  always @(*)
+    begin
+      case(pc)
+        4'h0:
+          inst = 8'h8a; // li r0, 10
+        4'h1:
+          inst = 8'h90; // li r1, 0
+        4'h2:
+          inst = 8'ha0; // li r2, 0
+        4'h3:
+          inst = 8'hb1; // li r3, 1
+        4'h4:
+          inst = 8'h17; // add r1, r1, r3
+        4'h5:
+          inst = 8'h29; // add r2, r2, r1
+        4'h6:
+          inst = 8'hd1; // bner0 r1, 4
+        4'h7:
+          inst = 8'h42; // out r2
+        default:
+          inst = 8'h00;
+      endcase
+    end
+
+  // 译码
+  wire [1:0] op   = inst[7:6];
+  wire [1:0] rd   = inst[5:4];
+  wire [1:0] rs1  = inst[3:2];
+  wire [1:0] rs2  = inst[1:0];
+  wire [3:0] imm  = inst[3:0];
+  wire [3:0] addr = inst[5:2];
+
+  // 2. 寄存器堆 (Register File)
+  reg [7:0] rf [3:0];
+
+  // 3. 执行与写回
+  always @(posedge clk)
+    begin
+      if (rst)
+        begin
+          pc <= 4'h0;
+          rf[0] <= 8'h0;
+          rf[1] <= 8'h0;
+          rf[2] <= 8'h0;
+          rf[3] <= 8'h0;
+          out_val <= 8'h0;
+        end
+      else
+        begin
+          case(op)
+            2'b00:
+              begin // add rd, rs1, rs2
+                rf[rd] <= rf[rs1] + rf[rs2];
+                pc <= pc + 1;
+              end
+            2'b01:
+              begin // out rs2
+                out_val <= rf[rs2];
+                pc <= pc + 1;
+              end
+            2'b10:
+              begin // li rd, imm
+                rf[rd] <= {4'b0, imm};
+                pc <= pc + 1;
+              end
+            2'b11:
+              begin // bner0 rs2, addr
+                if (rf[0] != rf[rs2])
+                  pc <= addr;
+                else
+                  pc <= pc + 1;
+              end
+          endcase
+        end
+    end
+
+endmodule
