8643fa1d73f271b9acd7ffa63be9679b55d713a5
tracer-ysyx <tracer@ysyx.org>
2026-02-13 14:48:55 +0800

    > sim RTL ysyx_22040000 黄伟健 Linux wiki-X99 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:55 up  2:32,  1 user,  load average: 3.13, 2.91, 2.50

---
+++ npc/ex6/vsrc/ex6.v
@@ -0,0 +1,26 @@
+module ex6(
+    input btn_clk,        // 按钮作为时钟信号
+    input btn_reset,       // 按钮作为复位信号
+    output [7:0] seg1,    // 数码管1 (显示高4位)
+    output [7:0] seg0     // 数码管0 (显示低4位)
+  );
+
+  wire [7:0] random_val;
+
+  lfsr my_lfsr(
+         .clk(btn_clk),
+         .reset(btn_reset),
+         .q(random_val)
+       );
+
+  bcd7seg seg_high(
+            .b(random_val[7:4]),
+            .h(seg1)
+          );
+
+  bcd7seg seg_low(
+            .b(random_val[3:0]),
+            .h(seg0)
+          );
+
+endmodule
---
+++ npc/ex6/vsrc/lfsr.v
@@ -0,0 +1,23 @@
+module lfsr(
+    input clk,
+    input reset,
+    output reg [7:0] q
+  );
+
+  wire feedback;
+
+  assign feedback = q[4] ^ q[3] ^ q[2] ^ q[0];
+
+  always @(posedge clk)
+    begin
+      if (reset)
+        begin
+          q <= 8'h01;
+        end
+      else
+        begin
+          q <= {feedback, q[7:1]};
+        end
+    end
+
+endmodule
