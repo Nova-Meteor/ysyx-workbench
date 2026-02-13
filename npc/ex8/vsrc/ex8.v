6892cf381801ee910bdc684adf6bf9d5bcd43aa5
tracer-ysyx <tracer@ysyx.org>
2026-02-13 19:56:50 +0800

    > sim RTL ysyx_22040000 黄伟健 Linux wiki-X99 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:50 up 46 min,  1 user,  load average: 1.96, 2.38, 2.41

--- npc/ex8/vsrc/ex8.v
+++ npc/ex8/vsrc/ex8.v
@@ -32,7 +32,7 @@ module ex8(
 
   vmem my_vmem(
          .h_addr(h_addr),
-         .v_addr(v_addr[8:0]),
+         .v_addr(v_addr),
          .vga_data(vga_data)
        );
 
@@ -40,15 +40,15 @@ endmodule
 
 module vmem(
     input [9:0] h_addr,
-    input [8:0] v_addr,
+    input [9:0] v_addr,
     output [23:0] vga_data
   );
 
-  reg [23:0] vga_mem [524287:0];
+  reg [23:0] vga_mem [1048575:0];
 
   initial
     begin
-      $readmemh("/home/wiki/ysyx/npc/ex8/resource/picture.hex", vga_mem);
+      $readmemh("/home/wiki/ysyx/npc/ex8/resource/ysyx.hex", vga_mem);
     end
 
   assign vga_data = vga_mem[{h_addr, v_addr}];
