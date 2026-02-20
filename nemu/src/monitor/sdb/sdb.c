e120f2d8ddf680c7e97397a7940c4a1066c2f462
tracer-ysyx <tracer@ysyx.org>
2026-02-20 20:55:41 +0800

    > compile NEMU ysyx_22040000 黄伟健 Linux wiki-X99 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:55:41 up  5:21,  1 user,  load average: 4.25, 3.08, 2.52

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
 static struct {
   const char *name;
   const char *description;
@@ -63,6 +65,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Step into one instruction", cmd_si },
 
   /* TODO: Add more commands */
 
@@ -90,6 +93,15 @@ static int cmd_help(char *args) {
     }
     printf("Unknown command '%s'\n", arg);
   }
+  return 0;
+}
+
+static int cmd_si(char *args) {
+  int n = 1;
+  if (args != NULL) {
+    sscanf(args, "%d", &n);
+  }
+  cpu_exec(n);
   return 0;
 }
 
